5add2bcaa6411ff6a30b6848b57b37ae362e9a80
abdelrahmanMohamed <abdo.mohamed.am.ao@gmail.com>
2018-10-25 12:34:04 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FullCalender.Models;
+
+namespace FullCalender.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+            using (MyDataBaseEntities dc = new MyDataBaseEntities())
+            {
+                var events = dc.Table_Event.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
